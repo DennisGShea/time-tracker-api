0daac435577e4631653bcdb51ca88545ff6f6fb2
Dennis G Shea <DennisGSheaPhD@gmail.com>
2021-02-17 17:50:26 -0500

    added post and get activities

--- functions/index.js
+++ functions/index.js
@@ -2,24 +2,14 @@ const functions = require("firebase-functions");
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const { getActivities, postActivity } = require('./src/activities')
 
 const app = express() 
 app.use(bodyParser.json())
 app.use(cors())
 
-app.get('/activities', (req, res) => {
-    // get activities from firestore
-    // form the documents we get back into a json array 
-    // [optional: 
-    // send json 
-res.status(200).json([
-    { name: 'Client Work',
-    totalTime: 900,
-},{
-    name: 'Personal Project',
-    totalTime: 750,
-}])
-})
+app.get('/activities', getActivities)
+app.post('/activities', postActivity)
 
 exports.app = functions.https.onRequest(app)
 
