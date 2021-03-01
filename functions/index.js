e3b80d17833f3f9bfd4ae29ab7df20649102c22b
Dennis G Shea <DennisGSheaPhD@gmail.com>
2021-02-28 22:01:00 -0500

    deployed this ver to Firebase

--- functions/index.js
+++ functions/index.js
@@ -1,15 +1,16 @@
-const functions = require("firebase-functions");
+const functions = require("firebase-functions")
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
-const { getActivities, postActivity } = require('./src/activities')
+const { getActivities, postActivity, addTimeTracking } = require('./src/activities')
 
-const app = express() 
+const app = express()
 app.use(bodyParser.json())
 app.use(cors())
 
 app.get('/activities', getActivities)
 app.post('/activities', postActivity)
+app.patch('/activities/:activityId', addTimeTracking)
 
 exports.app = functions.https.onRequest(app)
 
--- functions/src/activities/index.js
+++ functions/src/activities/index.js
@@ -1,5 +1,5 @@
-const admin = require("firebase-admin");
-const serviceAccount = require("../../credentials.json");
+const admin = require('firebase-admin')
+const serviceAccount = require('../../credentials.json')
 
 let db;
 
@@ -7,48 +7,66 @@ function authDB() {
   if (!admin.apps.length) {
     admin.initializeApp({
       credential: admin.credential.cert(serviceAccount),
-    });
-    db = admin.firestore();
+    })
+    db = admin.firestore()
   }
 }
 
 exports.getActivities = (req, res) => {
-  authDB();
-  db.collection("activities")
-    .get() //.where..
-    .then((collection) => {
-      const activityResults = collection.docs.map((doc) => {
-        let activity = doc.data();
-        activity.id = doc.id;
-        return activity;
-      });
-      res.status(200).json(activityResults);
+  authDB()
+  db.collection('activities').get() //.where('userId','==', userId)
+    .then(collection => {
+        const activityResults = collection.docs.map(doc => {
+          let activity = doc.data()
+          activity.id = doc.id
+          return activity
+        })
+        res.status(200).json(activityResults)
     })
-    .catch((e) => {
-      res.status(500).send("Error retrieving activities:" + e);
-    });
-};
+    .catch(e => {
+      res.status(500).send('Error retrieving activities: ' + e)
+    })
+}
+
+exports.addTimeTracking = (req, res) => {
+  if(!req.body || !req.body.duration || !req.params.activityId) {
+    res.status(400).send('Invalid request')
+  }
+  authDB()
+  const now = admin.firestore.FieldValue.serverTimestamp()
+  db.collection('activities').doc(req.params.activityId).update({
+    updated: now,
+    logs: admin.firestore.FieldValue.arrayUnion(req.body),
+    totalDuration: admin.firestore.FieldValue.increment(parseInt(req.body.duration))
+  })
+  .then(() => {
+    this.getActivities(req, res)
+  })
+  .catch(err => {
+    console.log(err)
+    res.status(500).send('Error updating activity: ' + err)
+  })
+}
 
 exports.postActivity = (req, res) => {
-  if (!req.body || !req.body.name || !req.body.userId) {
-    res.status(401).send("Missing new Activity data");
+  if(!req.body || !req.body.name || !req.body.userId){
+    res.status(401).send('Missing new Activity data')
   }
-  authDB();
-  const now = admin.firestore.FieldValue.serverTimestamp();
+  authDB()
+  const now = admin.firestore.FieldValue.serverTimestamp()
   let newActivity = {
     name: req.body.name,
-    totalDuraction: 0,
-    userID: req.body.userId,
+    totalDuration: 0,
+    userId: req.body.userId,
     logs: [],
     created: now,
     updated: now,
-  };
-  db.collection("activities")
-    .add(newActivity)
+  }
+  db.collection('activities').add(newActivity)
     .then(() => {
-      this.getActivities(req, res);
+      this.getActivities(req, res)
     })
-    .catch((e) => {
-      res.status(500).send("error....." + e);
-    });
-};
+    .catch(e => {
+      res.status(500).send('Error creating new Activity: ' + e)
+    })
+}
